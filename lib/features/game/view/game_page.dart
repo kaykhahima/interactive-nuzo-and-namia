b5261b86acfd2489de1e3f07aad97e82a3f87334
Kay Khahima <kaykhahima@gmail.com>
2024-08-18 11:15:11 +0300

    load items and combinations on game start

--- lib/features/game/view/game_page.dart
+++ lib/features/game/view/game_page.dart
@@ -11,24 +11,70 @@ import '../view_model/game_view_model.dart';
 /// If the opening scene has not been played, the video screen is shown playing the opening scene
 /// If the opening scene has ended and the game is not yet completed, the game play screen is shown. This is where the player selects the correct combination
 /// If the game has been completed, the video screen is shown playing the closing scene
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  late Future _loadDataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadDataFuture = _loadData();
+  }
+
+  //get combinations and items
+  Future _loadData() async {
+    try {
+      final gameViewModel = context.read<GameViewModel>();
+
+      // Fetch combinations and set initial combination
+      await gameViewModel.getCombinations();
+      gameViewModel.setInitialCombination();
+
+      // Fetch items
+      await gameViewModel.getItems();
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: SafeArea(
-        child: Consumer<GameViewModel>(builder: (context, gameViewModel, _) {
-          return gameViewModel.hasOpeningSceneEnded &&
-                  !gameViewModel.hasGameBeenCompleted
-              ? const GamePlayScreen()
-              : VideoScreen(
-                  scene: gameViewModel.hasGameBeenCompleted
-                      ? VideoScene.closeScene
-                      : VideoScene.openScene,
+        child: FutureBuilder(
+            future: _loadDataFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
                 );
-        }),
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text(snapshot.error.toString()),
+                );
+              }
+
+              return Consumer<GameViewModel>(
+                  builder: (context, gameViewModel, _) {
+                return gameViewModel.hasOpeningSceneEnded &&
+                        !gameViewModel.hasGameBeenCompleted
+                    ? const GamePlayScreen()
+                    : VideoScreen(
+                        scene: gameViewModel.hasGameBeenCompleted
+                            ? VideoScene.closeScene
+                            : VideoScene.openScene,
+                      );
+              });
+            }),
       ),
     );
   }
--- lib/features/game/view/widgets/game_play_screen.dart
+++ lib/features/game/view/widgets/game_play_screen.dart
@@ -1,69 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../view_model/game_view_model.dart';
 import 'combination_list.dart';
 import 'item_list.dart';
 
-class GamePlayScreen extends StatefulWidget {
+class GamePlayScreen extends StatelessWidget {
   const GamePlayScreen({super.key});
 
   @override
-  State<GamePlayScreen> createState() => _GamePlayScreenState();
-}
-
-class _GamePlayScreenState extends State<GamePlayScreen> {
-  late Future _loadDataFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadDataFuture = _loadData();
-  }
-
-  Future _loadData() async {
-    try {
-      final gameViewModel = context.read<GameViewModel>();
-
-      // Fetch combinations and set initial combination
-      await gameViewModel.getCombinations();
-      gameViewModel.setInitialCombination();
-
-      // Fetch items
-      await gameViewModel.getItems();
-    } catch (e) {
-      return Future.error(e);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _loadDataFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          } else {
-            return const Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CombinationList(),
-                  SizedBox(height: 12.0),
-                  ItemList(),
-                  SizedBox(height: 24.0),
-                ],
-              ),
-            );
-          }
-        });
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CombinationList(),
+          SizedBox(height: 12.0),
+          ItemList(),
+          SizedBox(height: 24.0),
+        ],
+      ),
+    );
   }
 }
